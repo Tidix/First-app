2e4dc517d232b9d24523fd0f84455639cccd20fa
Damien <damien.lvzl@gmail.com>
2020-01-29 10:14:30 +0100

    Avancement de l'exo1

--- app/utils/note-class.js
+++ app/utils/note-class.js
@@ -3,6 +3,28 @@ import EmberObject, { computed } from '@ember/object';
 
 export default Route.extend({
     MAX:100,
-    content:'Entrez votre texte'
-    
+    content:'Entrez votre texte',
+
+    size : computed('content', 'MAX', function(){
+        let content = this.get('content');
+        let MAX = this.get('MAX');  
+        return `${MAX - content.length}`;
+    }),
+
+    info : computed('content', function(){
+
+    }),
+
+    style : computed('content', 'MAX',function(){
+        
+    }),
+
+    save : computed('content',function(){
+        
+    }),
+
+    clear : computed('content', function(){
+        
+    })
+
 });
