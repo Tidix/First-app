ee1a8bf1b5c1d5ab32f2f2350817015c19c47897
Damien <damien.lvzl@gmail.com>
2020-02-05 14:38:02 +0100

    changements

--- app/routes/client.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-    model(){
-        return{ nom:'Alpha Man',
-                detail:'Le plus grand de tous les héros'}
-    }
-});
--- app/routes/ex1.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-import Note from 'first-app/utils/note-class'
-export default Route.extend({
-    model(){
-        return Note.create();
-    }
-});
-    
